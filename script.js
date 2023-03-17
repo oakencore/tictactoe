a1bb1868a51b8e4d2616b57df8a91335524107fd
oakencore <105373906+oakencore@users.noreply.github.com>
2023-03-17 15:09:00 +0700

    Added all code into one giant object to code and added reset button

--- script.js
+++ script.js
@@ -1,100 +1,136 @@
-// Factory function to create new players
-function createPlayer(name, symbol) {
-  return {
-    name,
-    symbol,
-    score: 0,
-  };
-}
+const ticTacToeGame = {
+  player1: null,
+  player2: null,
+  currentPlayer: null,
+  movesPlayed: 0,
+  gameEnded: false,
+  gameBoardArray: [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ],
 
-const player1 = createPlayer("Player 1", "X");
-const player2 = createPlayer("Player 2", "O");
+  init() {
+    this.player1 = this.createPlayer("Player 1", "X");
+    this.player2 = this.createPlayer("Player 2", "O");
+    this.currentPlayer = this.player1;
+    this.createGameBoard();
+  },
 
-let currentPlayer = player1;
-let movesPlayed = 0;
-let gameEnded = false;
+  // Factory function to create players
+  createPlayer(name, symbol) {
+    return {
+      name,
+      symbol,
+      score: 0,
+    };
+  },
 
-const gameBoardArray = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
-];
+  // Handle cell clicks
+  cellClicked(event) {
+    if (this.gameEnded) return;
 
-// Winning conditions for the game.
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+    const cell = event.target;
+    const row = parseInt(cell.getAttribute("data-row"));
+    const col = parseInt(cell.getAttribute("data-col"));
 
-// Handle cell click event
-function cellClicked(event) {
-  if (gameEnded) return;
+    if (this.gameBoardArray[row][col] !== "") return;
 
-  const cell = event.target;
-  const row = parseInt(cell.getAttribute("data-row"));
-  const col = parseInt(cell.getAttribute("data-col"));
+    this.gameBoardArray[row][col] = this.currentPlayer.symbol;
+    cell.textContent = this.currentPlayer.symbol;
+    this.movesPlayed++;
 
-  if (gameBoardArray[row][col] !== "") return;
-
-  gameBoardArray[row][col] = currentPlayer.symbol;
-  cell.textContent = currentPlayer.symbol;
-  movesPlayed++;
-
-  if (checkWinner()) {
-    alert(`${currentPlayer.name} wins!`);
-    gameEnded = true;
-  } else if (movesPlayed === 9) {
-    alert("It's a draw!");
-    gameEnded = true;
-  } else {
-    currentPlayer = currentPlayer.symbol === "X" ? player2 : player1;
-  }
-}
-
-// Create the game board dynamically and add click event listeners to cells
-function createGameBoard() {
-  const gameBoard = document.getElementById("gameBoard");
-
-  for (let row = 0; row < 3; row++) {
-    const rowElement = document.createElement("div");
-    rowElement.classList.add("row");
-
-    for (let col = 0; col < 3; col++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.setAttribute("data-row", row);
-      cell.setAttribute("data-col", col);
-      cell.addEventListener("click", cellClicked);
-
-      rowElement.appendChild(cell);
+    if (this.checkWinner()) {
+      alert(`${this.currentPlayer.name} wins!`);
+      this.gameEnded = true;
+    } else if (this.movesPlayed === 9) {
+      alert("It's a draw!");
+      this.gameEnded = true;
+    } else {
+      this.currentPlayer =
+        this.currentPlayer === this.player1 ? this.player2 : this.player1;
     }
+  },
 
-    gameBoard.appendChild(rowElement);
-  }
-}
+  // Create the grid and cells
+  createGameBoard() {
+    const gameBoard = document.getElementById("gameBoard");
 
-// Check if the current player has won
-function checkWinner() {
-  const flattenedBoard = gameBoardArray.flat();
+    for (let row = 0; row < 3; row++) {
+      const rowElement = document.createElement("div");
+      rowElement.classList.add("row");
 
-  for (const condition of winningConditions) {
-    const [a, b, c] = condition;
-    if (
-      flattenedBoard[a] === currentPlayer.symbol &&
-      flattenedBoard[a] === flattenedBoard[b] &&
-      flattenedBoard[a] === flattenedBoard[c]
-    ) {
-      return true;
+      for (let col = 0; col < 3; col++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.setAttribute("data-row", row);
+        cell.setAttribute("data-col", col);
+        cell.addEventListener("click", (event) => this.cellClicked(event));
+
+        rowElement.appendChild(cell);
+      }
+
+      gameBoard.appendChild(rowElement);
     }
-  }
-  return false;
-}
+  },
 
-// Call the createGameBoard function to create the grid
-createGameBoard();
+  // Winning conditions for 1d grid.
+  checkWinner() {
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    const flattenedBoard = this.gameBoardArray.flat(); //Flattened turning the array from 2 to 1D.
+
+    for (const condition of winningConditions) {
+      const [a, b, c] = condition;
+      if (
+        flattenedBoard[a] === this.currentPlayer.symbol &&
+        flattenedBoard[a] === flattenedBoard[b] &&
+        flattenedBoard[a] === flattenedBoard[c]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  },
+
+  restart() {
+    // Event listener for restart button
+    const restartButton = document.getElementById("restartButton");
+    restartButton.addEventListener("click", () => {
+      this.restartGame();
+    });
+  },
+
+  restartGame() {
+    // Clears symbols from gameboardarray
+    this.gameBoardArray = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+
+    // Change player to player1 and clear moves. 
+    this.currentPlayer = this.player1;
+    this.movesPlayed = 0;
+    this.gameEnded = false;
+
+    // Empty content from all cells
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.textContent = "";
+    });
+  },
+};
+
+// Initialise the game
+ticTacToeGame.init();
+ticTacToeGame.restart();
